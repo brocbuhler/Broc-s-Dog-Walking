c4637a58a10fc086c79d8124da93dd175c3e5ab9
Broc Buhler <brocbuhler@gmail.com>
2025-08-12 16:57:53 -0700

    Was messing around don't know what I changed

--- client/src/Home.jsx
+++ client/src/Home.jsx
@@ -2,9 +2,7 @@ import { getGreeting } from "./apiManager";
 import { useEffect, useState } from "react";
 
 export default function Home() {
-  const [greeting, setGreeting] = useState({
-    message: "Not Connected to the API",
-  });
+  const [greeting, setGreeting] = useState({});
 
   useEffect(() => {
     getGreeting()
